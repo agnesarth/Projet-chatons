4389452804b3a5f0bc28c0085024fd7694ffb0c4
agnesarth <agnesarth@hotmail.fr>
2020-06-01 15:49:53 +0200

    controllers carts and orders are created

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,22 +1,36 @@
 class CartsController < ApplicationController
   def new
+    @cart = Cart.new
   end
 
   def create
   end
 
   def show
+    @cart = Cart.find(params[:id])
   end
 
   def index
+    @carts = Cart.all
   end 
 
   def edit
+    @cart = Cart.find(params[:id])
   end
 
   def update
+    @cart = Cart.find(params[:id])
+    @cart.update(cart_params)
   end
 
   def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).
   end
 end
