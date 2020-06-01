994b14020bd91d757265ac9e125f91091e5a8177
agnesarth <agnesarth@hotmail.fr>
2020-06-01 16:58:36 +0200

    small changes in controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  after_create :welcome_send
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+      @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
+  end
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+end
