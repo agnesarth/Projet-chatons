b79194c7916520d73ca351aa2e16592968abd5e9
Raphael-dln <delannay.raphael@gmail.com>
2020-06-01 18:21:38 +0200

    update models + footer

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :cart_items
+  has_many :carts, through: :cart_items
+  validates :title, presence: true, length: { in: 5..45 }
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :price, presence: true, inclusion: { in: 1..1000}
+  validates :image_url, presence: true
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
     has_one :cart
+    belongs_to :user
 end
