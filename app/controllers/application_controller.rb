79894c6708f71c6e23c1916c0fb9c5bc1a482a71
mil3zh <milena.lazzaretti5@gmail.com>
2020-06-02 13:39:05 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,2 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-
-  def current_user
-    if session[:user_id]
-      current_user = User.find(session[:user_id])
-    end
-  end
-
-  def current_cart
-    if !session[:cart_id].nil?
-      current_cart = Cart.find(session[:cart_id])
-    else
-      current_cart = Cart.new
-    end
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
 
-  resources :orders,:carts,:items
+  resources :users,:orders,:carts,:items
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
