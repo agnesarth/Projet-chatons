bdff5f79ca260b5ae1a5ff6726a6b602930e8c10
lgentaz <lucile.gentaz@gmail.com>
2020-06-03 08:40:50 +0200

    a bit more contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,14 @@ class StaticPagesController < ApplicationController
   
     def home
     end
+
+    def contact
+      first_name = params[:first_name]
+      last_name = params[:last_name]
+      email = params[:email]
+      subject = params[:subject]
+      data = params[:body]
+      Mailer.contact_email(first_name, last_name, email, subject, data).deliver
+    end
+
   end
