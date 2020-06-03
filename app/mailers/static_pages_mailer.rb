5cfcab773142d3024d20164035524b852e152014
lgentaz <lucile.gentaz@gmail.com>
2020-06-04 00:34:13 +0200

    merging mailer

--- app/mailers/static_pages_mailer.rb
+++ app/mailers/static_pages_mailer.rb
@@ -1,13 +1,8 @@
 class StaticPagesMailer < ApplicationMailer
-<<<<<<< HEAD
-    def contact_email(first_name, last_name, email, subject, data)
-        @user = last_name
-=======
     default from: 'animalmatching2020@gmail.com'
 
     def contact_email(name, email, subject, data)
         @user = name
->>>>>>> mailer
         @user_email = email
         @body = data
         mail(to: 'animalmatching2020@yopmail.com', subject: subject)
