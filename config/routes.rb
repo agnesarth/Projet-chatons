c99c70082fe41b01bb7b7e724d0becb094e5922d
Raphael-dln <delannay.raphael@gmail.com>
2020-06-04 12:35:54 +0200

    routes updates : cleaning + rename

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   root to: 'items#index'
   devise_for :users
 
-  resources :users,:carts,:items, :charges
-
-  resources :users do
-    resources :orders
+  resources :carts, :items
+  
+  resources :users, path: "mon_compte" do
+    resources :orders, path: "mes_commandes"
   end
   
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
 end
